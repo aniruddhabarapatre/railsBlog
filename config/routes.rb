a56a7422ed6992bd728b44d4c8c50c8b360c2318
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2014-03-30 00:34:19 -0500

    Update route and create view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 RailsBlog::Application.routes.draw do
 
-  resources :posts
-  root :to => "posts#index"
+  resources :post
+  root :to => "post#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
